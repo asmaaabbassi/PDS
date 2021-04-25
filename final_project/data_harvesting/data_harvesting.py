a38d352ccecd75e9dbd31cbc1ddd93ec45441e14
MonnierThomas <54896884+MonnierThomas@users.noreply.github.com>
2021-04-25 17:25:51 +0200

    Update data_harvesting.py

--- final_project/data_harvesting/data_harvesting.py
+++ final_project/data_harvesting/data_harvesting.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[44]:
-
-
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
@@ -13,55 +7,52 @@ from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-driver.get("https://www.investing.com/indices/usdollar")
+def data_harvesting(file):
+  driver.get(file)
 
-element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, "onetrust-accept-btn-handler")))
-python_button = driver.find_elements_by_xpath('//*[@id="onetrust-accept-btn-handler"]')[0]
-python_button.click()
+  element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, "onetrust-accept-btn-handler")))
+  python_button = driver.find_elements_by_xpath('//*[@id="onetrust-accept-btn-handler"]')[0]
+  python_button.click()
 
-driver.execute_script("window.scrollTo(0, 200)")
-driver.find_element_by_link_text("Historical Data").click()
+  driver.execute_script("window.scrollTo(0, 200)")
+  driver.find_element_by_link_text("Historical Data").click()
 
-driver.find_element_by_link_text("Sign In").click()
+  driver.find_element_by_link_text("Sign In").click()
 
-mail = driver.find_elements_by_xpath('//*[@id="loginFormUser_email"]')[0]
-mail.click()
-mail.clear()
-mail.send_keys('thomas.monnier@mines-paristech.fr')
+  mail = driver.find_elements_by_xpath('//*[@id="loginFormUser_email"]')[0]
+  mail.click()
+  mail.clear()
+  mail.send_keys('thomas.monnier@mines-paristech.fr')
 
-password = driver.find_elements_by_xpath('//*[@id="loginForm_password"]')[0]
-password.click()
-password.clear()
-password.send_keys('Project2021!')
+  password = driver.find_elements_by_xpath('//*[@id="loginForm_password"]')[0]
+  password.click()
+  password.clear()
+  password.send_keys('Project2021!')
+  password.send_keys(Keys.ENTER)
 
-password.send_keys(Keys.ENTER)
+  driver.execute_script("window.scrollTo(0, 500)")
 
+  python_span = driver.find_elements_by_xpath('//*[@id="datePickerIconWrap"]/span')[1]
+  python_span.click()
 
-driver.execute_script("window.scrollTo(0, 500)")
+  begin_date = driver.find_elements_by_xpath('//*[@id="startDate"]')[0]
+  begin_date.click()
+  begin_date.clear()
+  begin_date.send_keys('01/01/2020')
 
-python_span = driver.find_elements_by_xpath('//*[@id="datePickerIconWrap"]/span')[1]
-python_span.click()
+  end_date = driver.find_elements_by_xpath('//*[@id="endDate"]')[0]
+  end_date.click()
+  end_date.clear()
+  end_date.send_keys('12/31/2020')
 
-begin_date = driver.find_elements_by_xpath('//*[@id="startDate"]')[0]
-begin_date.click()
-begin_date.clear()
-begin_date.send_keys('01/01/2020')
+  driver.find_element_by_link_text("Apply").click()
 
-end_date = driver.find_elements_by_xpath('//*[@id="endDate"]')[0]
-end_date.click()
-end_date.clear()
-end_date.send_keys('12/31/2020')
-
-driver.find_element_by_link_text("Apply").click()
-
-driver.find_element_by_link_text("Download Data").click()
-
-
-#driver.close()
+  driver.find_element_by_link_text("Download Data").click()
 
 
-# In[ ]:
+files = ["https://www.investing.com/funds/amundi-msci-wrld-ae-c", "https://www.investing.com/etfs/ishares-global-corporate-bond-$", "https://www.investing.com/etfs/db-x-trackers-ii-global-sovereign-5", "https://www.investing.com/etfs/spdr-gold-trust", "https://www.investing.com/indices/usdollar"]
 
-
+for file in files:
+  data_harvesting(file)
 
 
